916902542740d3a35d6a42488fe51b0f0e4fb1b7
Peach Zhang <CrazyCatZhang@outlook.com>
2022-05-24 21:14:22 +0800

    Base use of useEffect hook

--- expand/src/components/Hooks/index.jsx
+++ expand/src/components/Hooks/index.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import {root} from "../../index";
 
 function Hooks() {
 
     const [count, setCount] = React.useState(0)
 
+    React.useEffect(() => {
+        let timer = setInterval(() => {
+            setCount(count => count + 1)
+        }, 1000)
+        return () => {
+            clearInterval(timer)
+        }
+    }, [])
+
     function add() {
         setCount(count + 1)
     }
 
+    function unmount() {
+        root.unmount()
+    }
+
     return (
-        <div>
+        <div id="hooks" className="hooks">
             <h2>当前求和为：{count}</h2>
             <button onClick={add}>点我加1</button>
+            <button onClick={unmount}>卸载组件</button>
         </div>
     );
 }
