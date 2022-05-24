a29f45b429a3c79cc80fa280d2883ff9149dc8c5
Peach Zhang <CrazyCatZhang@outlook.com>
2022-05-24 18:46:46 +0800

    Base use of lazy-loaded

--- expand/src/App.js
+++ expand/src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import SetState from "./components/SetState";
+import LazyLoad from "./components/LazyLoad";
 
 class App extends Component {
     render() {
         return (
             <div>
-              <SetState/>
+                <SetState/>
+                <hr/>
+                <LazyLoad/>
             </div>
         );
     }
