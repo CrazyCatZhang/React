96f3d75567e29f02a34c939f18a0fc43e74ee6f9
Peach Zhang <CrazyCatZhang@outlook.com>
2022-05-24 21:26:06 +0800

    Base use of useRef hook

--- expand/src/components/Hooks/index.jsx
+++ expand/src/components/Hooks/index.jsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 function Hooks() {
 
     const [count, setCount] = React.useState(0)
+    
+    const myRef = React.useRef()
 
     React.useEffect(() => {
         let timer = setInterval(() => {
@@ -21,12 +23,18 @@ function Hooks() {
     function unmount() {
         ReactDOM.createRoot(document.getElementById('hooks')).unmount()
     }
+    
+    function show() {
+        alert(myRef.current.value)
+    }
 
     return (
         <div id="hooks" className="hooks">
+            <input type="text" ref={myRef}/>
             <h2>当前求和为：{count}</h2>
             <button onClick={add}>点我加1</button>
             <button onClick={unmount}>卸载组件</button>
+            <button onClick={show}>点我展示</button>
         </div>
     );
 }
